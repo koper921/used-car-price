6560efd76497081f6ac4ec49295a3626edaed422
des Lauriers <cgdeslauriers@gmail.com>
2019-01-30 15:04:50 +0100

    remove outliers in problem.py read data

--- problem.py
+++ problem.py
@@ -2,11 +2,12 @@
 import os
 import pandas as pd
 import rampwf as rw
-from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.model_selection import ShuffleSplit
+import numpy as np
 
 problem_title = 'Cars price'
 _target_column_names = 'price'
-_ignore_column_names = ["postalCode", "name" ]
+_ignore_column_names = ["postalCode" ]
 #_prediction_label_names = [0, 1]
 
 Predictions = rw.prediction_types.make_regression(
@@ -29,12 +30,22 @@ score_types = [
 
 
 def get_cv(X, y):
-    cv = StratifiedShuffleSplit(n_splits=8, test_size=0.2, random_state=57)
-    return cv.split(X, y)
+    bins     = np.linspace(0, max(y), 3)
+    y_binned = np.digitize(y, bins)
+    #X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y_binned)
+    #cv = ShuffleSplit(n_splits=8, test_size=0.2)
+    cv = ShuffleSplit(n_splits=8, test_size=0.2, random_state=57)
+    return cv.split(X, y_binned)
 
 
 def _read_data(path, f_name):
     data = pd.read_csv(os.path.join(path, 'data', f_name))
+    # filter outliers 
+    data = data[
+    (data["yearOfRegistration"].between(1945, 2017, inclusive=True)) &
+    (data["powerPS"].between(100, 500, inclusive=True)) &
+    (data["price"].between(100, 200000, inclusive=True))
+        ]
     y_array = data[_target_column_names].values
     X_df = data.drop([_target_column_names] + _ignore_column_names, axis=1)
     return clean_and_transform(X_df), y_array
@@ -53,6 +64,9 @@ def clean_and_transform(X):
     data = X.copy()
     
     # Replace the NaN-Values
+    data['namelen'] = [min(70, len(n)) for n in data['name']]
+    
+
     data['vehicleType'].fillna(value='blank', inplace=True)
     data['gearbox'].fillna(value='blank', inplace=True)
     data['model'].fillna(value='blank', inplace=True)
@@ -67,7 +81,10 @@ def clean_and_transform(X):
     cat_columns = data.select_dtypes(['category']).columns
 
     data[cat_columns] = data[cat_columns].apply(lambda x: x.cat.codes)
+    data = data.drop(['name' ], axis=1)
     
+    
+
     return data
 
 def drop_column(X, columns_to_drop=["dateCrawled", "abtest", "dateCreated", "lastSeen"]):
